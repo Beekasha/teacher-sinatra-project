6b9a6705a02562235785b3369c0d1a14c637be2f
Beekasha <brian.j.kleiner@gmail.com>
2020-03-16 13:53:00 -0400

    AddTeacherIdColumnToKlasses migration made

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200316174453) do
+ActiveRecord::Schema.define(version: 20200316175124) do
 
   create_table "klasses", force: :cascade do |t|
     t.string  "name"
     t.integer "period"
+    t.integer "teacher_id"
   end
 
   create_table "reviews", force: :cascade do |t|
