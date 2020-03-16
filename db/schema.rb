eabc89336cbf104283f160e359db33fdd7749418
Beekasha <brian.j.kleiner@gmail.com>
2020-03-16 00:21:11 -0400

    students table created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200316040928) do
+ActiveRecord::Schema.define(version: 20200316041444) do
+
+  create_table "students", force: :cascade do |t|
+    t.string  "name"
+    t.integer "grade"
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
