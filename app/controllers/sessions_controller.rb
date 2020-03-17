a291440a6044aaaa3e7360622d28fd441c2a2aad
Beekasha <brian.j.kleiner@gmail.com>
2020-03-17 14:43:01 -0400

    routes to home page fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,23 @@ class SessionsController < ApplicationController
         erb :'sessions/login'
     end
 
+    post "/login" do
+        @user = Teacher.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to "/account"
+        else
+            redirect to "sessions/failure"
+        end
+    end
+
     post '/sessions' do
         login(params[:username], params[:password])
-        redirect to '/reviews'
+        redirect to '/'
     end
 
     get '/logout' do
         logout!
-        redirect to '/reviews'
+        redirect to '/'
     end
 end
