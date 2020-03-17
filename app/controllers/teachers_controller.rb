89720e8960e6d8de72c28071c85b319feb1d4b35
Beekasha <brian.j.kleiner@gmail.com>
2020-03-17 13:38:15 -0400

    signup page skeleton fleshed out

---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,16 @@
+class TeachersController < ApplicationController
+    get '/signup' do
+        erb :"teachers/signup"
+    end
+
+    post '/teachers' do 
+        @teacher = Teacher.new
+        @teacher.username = params[:username]
+        @teacher.password = params[:password]
+        if @teacher.save
+            redirect '/login'
+        else
+            erb :"teachers/signup"
+        end
+    end
+end
