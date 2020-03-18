a15a01082367aa121960b642ea9fb1b1c061b6fd
Beekasha <brian.j.kleiner@gmail.com>
2020-03-18 01:25:37 -0400

    seeds updated without reviews

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -1,33 +1,44 @@
-5.times do 
+5.times do #creating 5 teachers
     fake_name = Faker::Name.name
     fake_email = Faker::Internet.email
     fake_password = Faker::Space.constellation
-    Teacher.create(name:, username:, password:)
+    Teacher.create(name: fake_name, username: fake_email, password: fake_password)
 end
 
 
 
 
-Teacher.all.each do |t|
+Teacher.all.each do |t| 
 
-
-  # create students
-  20.times do
-    first = Faker::Name.first_name
-    last = Faker::Name.last_name
-    name = "#{first} #{last}"
-    username = first[0] + last[0] + last
-    pass = Faker::Space.constellation
-    Student.create(name: name, username: username.downcase, password: pass, teacher_id: t.id)
+  20.times do #creating 20 students p/teacher
+    name = Faker::Name.name
+    grade = rand(1..12)
+   
+    Student.create(name: name, grade: grade)
   end
+
+  name = Faker::Educator.course_name #create klasses
+  period = rand(1..12)
+  teacher_id = t.id
+  max = Student.all.count
+  student_id = rand(1..max)
+  Klass.create(name: name, period: period, teacher_id: teacher_id, student_id: student_id)
 end
+
+# 5.times do #create klasses
+#     name = Faker::Educator.course_name
+#     period = rand(1..12)
+#     # teacher_id
+#     # student_id
+#     Klass.create(name:, period:, teacher_id:, student_id:)
+# end
 
 # Create student reviews
-Student.all.each do |s|
-  3.times do
-    review = Faker::Movies::HarryPotter.quote
-    t = Teacher.find_by(id: s.teacher_id)
-    book = t.books[rand(t.books.length)]
-    Review.create(rating: rating, review: review, student_id: s.id, book_id: book.id)
-  end
-end
+# Student.all.each do |s|
+#   3.times do
+#     review = Faker::Movies::BackToTheFuture.quote
+#     t = Teacher.find_by(id: s.teacher_id)
+#     book = t.books[rand(t.books.length)]
+#     Review.create(rating: rating, review: review, student_id: s.id, book_id: book.id)
+#   end
+# end
