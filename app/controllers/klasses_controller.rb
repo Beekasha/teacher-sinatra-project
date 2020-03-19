3b731845e97ef4ae7db709184f0f9b02c5cdab1f
Beekasha <brian.j.kleiner@gmail.com>
2020-03-19 16:01:02 -0400

    slug methods moved to Klass model

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -15,13 +15,7 @@ class KlassesController < ApplicationController
     end
 
 
-    
-    def slug
-        name.downcase.gsub(" ","-")
-    end
 
-    def self.find_by_slug(slug)
-        Klass.all.find{|k| k.slug == slug}
-    end
+
 
 end
--- app/models/Klass.rb
+++ app/models/Klass.rb
@@ -1,4 +1,12 @@
 class Klass < ActiveRecord::Base
     has_and_belongs_to_many :student
     belongs_to :teacher 
+
+    def slug
+        name.downcase.gsub(" ","-")
+    end
+
+    def self.find_by_slug(slug)
+        Klass.all.find{|k| k.slug == slug}
+    end
 end
